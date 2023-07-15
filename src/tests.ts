7495366fa12e4a25dcff2047bca37e5262b6cacd
Fabio Spampinato <spampinabio@gmail.com>
2023-07-15 16:25:53 +0100

    Added a todo

--- src/tests.ts
+++ src/tests.ts
@@ -6,6 +6,8 @@ import type {FunctionMaybe} from './types';
 
 /* MAIN */
 
+//TODO: Maybe an isLike kind of check should be performed instead while testing, to allow extra properties
+
 const anyOf = ( value: unknown, values: FunctionMaybe<unknown[]> ): boolean => {
 
   return resolve ( values ).some ( other => isEqual ( value, other ) );
